57c9864ec9f2ca397d472d814d2bfedb38ea2bf2
gh{}st <159590854+GambinoTDX@users.noreply.github.com>
2026-06-04 22:27:22 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Main.jsx'
 
+console.log('main loaded', App)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
